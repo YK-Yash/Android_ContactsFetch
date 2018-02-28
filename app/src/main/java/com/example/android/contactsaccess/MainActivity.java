02f751219d16392c1bbb9475c2225966b2b355c4
YK-Yash <yashkumar1998@gmail.com>
2018-02-28 13:16:29 +0530

    Got the contacts

---
+++ app/src/main/java/com/example/android/contactsaccess/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.contactsaccess;
+
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Build;
+import android.provider.ContactsContract;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayList<HashMap<String,String>> contactData=new ArrayList<HashMap<String,String>>();
+        ContentResolver cr = getContentResolver();
+        Cursor cursor = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI,null, null, null, null);
+        while (cursor.moveToNext()) {
+            try{
+                String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+                String name=cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+                String hasPhone = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER));
+                if (Integer.parseInt(cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.HAS_PHONE_NUMBER))) > 0) {
+                    Cursor phones = getContentResolver().query( ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, ContactsContract.CommonDataKinds.Phone.CONTACT_ID +" = "+ contactId, null, null);
+                    while (phones.moveToNext()) {
+                        String phoneNumber = phones.getString(phones.getColumnIndex( ContactsContract.CommonDataKinds.Phone.NUMBER));
+                        HashMap<String,String> map=new HashMap<String,String>();
+                        map.put("name", name);
+                        map.put("number", phoneNumber);
+                        contactData.add(map);
+                    }
+                    phones.close();
+                }
+            }catch(Exception e){}
+        }
+        try {
+            Toast.makeText(getBaseContext(),contactData.get(0)+"", Toast.LENGTH_LONG).show();
+
+        }catch (Exception e){
+            Toast.makeText(getBaseContext(),"No contacts", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+}
